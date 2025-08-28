75fd4b9c12da38feac92a206dbd07a2172d25cee
nawshad85 <abrarsami2021@gmail.com>
2025-08-29 01:06:59 +0600

    add supabse auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:loginpage/pages/signup.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'pages/loginpage.dart';
 import 'pages/homepage.dart';
 import 'pages/signup.dart';
 
-void main() {
+void main() async{
+  await Supabase.initialize(
+      url: "https://axnsovgjxnatvcmynwyh.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4bnNvdmdqeG5hdHZjbXlud3loIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxODYyNTcsImV4cCI6MjA3MTc2MjI1N30.hnOYcWnoAeqQHlm7fSFK3BBQoQ92xaos2pKPzQrL4UY"
+  );
   runApp(const MyApp());
 }
 
